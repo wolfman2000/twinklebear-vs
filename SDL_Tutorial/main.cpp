9584f64a928352a859c2b03224671a25f7d6800a
Jason Felds <jafelds@gmail.com>
2014-02-13 22:02:11 -0500

    Generic SDL Error Logger.

--- SDL_Tutorial/main.cpp
+++ SDL_Tutorial/main.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
+#include <string>
 #include <SDL.h>
 
 int const SCREEN_WIDTH = 640;
 int const SCREEN_HEIGHT = 480;
 
+void logSDLError(std::ostream &os, std::string const &msg) {
+	os << msg << " error: " << SDL_GetError() << std::endl;
+}
+
 int main(int argc, char **argv) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_Init");
 		return 1;
 	}
 
 	// TODO: Smart pointers at some points.
 	SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_CreateWindow");
 		return 1;
 	}
 
 	auto renderFlags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, renderFlags);
 	if (ren == nullptr) {
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_CreateRenderer");
 		return 1;
 	}
 
 	SDL_Surface *bmp = SDL_LoadBMP("hello.bmp");
 	if (bmp == nullptr) {
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_LoadBMP");
 		return 1;
 	}
 
@@ -34,7 +39,7 @@ int main(int argc, char **argv) {
 	// At this point, we no longer need the bmp surface.
 	SDL_FreeSurface(bmp);
 	if (tex == nullptr) {
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_CreateTextureFromSurface");
 		return 1;
 	}
 
