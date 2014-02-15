802b82dd71ef428aea9786f9452375f567b2906d
Jason Felds <jafelds@gmail.com>
2014-02-14 22:56:31 -0500

    Implementing the timer...with to_string instead.

--- SDL_Tutorial/main.cpp
+++ SDL_Tutorial/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Window.hpp"
+#include "Timer.hpp"
 
 int main(int argc, char **argv) {
 	try {
@@ -11,26 +12,25 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	SDL_Texture *img, *msg;
-	try {
-		std::string imgFile = "image.png";
-		img = Window::LoadTexture(imgFile);
+	Timer timer;
 
-		std::string fontFile = "SourceSansPro-Regular.ttf";
-		std::string text = "TTF Fonts too!";
-		SDL_Color color = { 255, 255, 255 };
-		msg = Window::RenderText(text, fontFile, color, 25);
-	}
-	catch (std::runtime_error const &e) {
-		std::cout << e.what() << std::endl;
-		Window::Quit();
-		return -1;
-	}
+	SDL_Texture *msg = nullptr, *ticks = nullptr;
+
+	SDL_Color white = { 255, 255, 255 };
+	SDL_Rect msgBox, ticksBox;
 
 	auto box = Window::Box();
-	SDL_Rect pos = { box.w / 2 - 150 / 2, box.h / 2 - 150 / 2, 150, 150 };
-	float angle = 0;
+	msg = Window::RenderText("Ticks Elapsed: ", "SourceSansPro-Regular.ttf", white, 30);
+	msgBox.x = 0;
+	msgBox.y = box.h / 2;
+	SDL_QueryTexture(msg, nullptr, nullptr, &msgBox.w, &msgBox.h);
 	
+	std::string strTicks = std::to_string(timer.Ticks());
+	ticks = Window::RenderText(strTicks.c_str(), "SourceSansPro-Regular.ttf", white, 30);
+	ticksBox.x = msgBox.w + 20;
+	ticksBox.y = box.h / 2;
+	SDL_QueryTexture(ticks, nullptr, nullptr, &ticksBox.w, &ticksBox.h);
+
 	SDL_Event evt;
 	bool quit = false;
 
@@ -44,11 +44,21 @@ int main(int argc, char **argv) {
 			else if (evt.type == SDL_KEYDOWN) {
 				// user presses any key.
 				switch (evt.key.keysym.sym) {
-				case SDLK_d:
-					angle += 2;
+				case SDLK_s:
+					if (timer.IsStarted()) {
+						timer.Stop();
+					}
+					else {
+						timer.Start();
+					}
 					break;
-				case SDLK_a:
-					angle -= 2;
+				case SDLK_p:
+					if (timer.IsPaused()) {
+						timer.Unpause();
+					}
+					else {
+						timer.Pause();
+					}
 					break;
 				case SDLK_ESCAPE:
 					quit = true;
@@ -59,16 +69,21 @@ int main(int argc, char **argv) {
 			}
 		}
 
+		if (timer.IsStarted() && !timer.IsPaused()) {
+			strTicks = std::to_string(timer.Ticks());
+			SDL_DestroyTexture(ticks);
+			ticks = Window::RenderText(strTicks.c_str(), "SourceSansPro-Regular.ttf", white, 30);
+			SDL_QueryTexture(ticks, nullptr, nullptr, &ticksBox.w, &ticksBox.h);
+		}
+
 		Window::Clear();
-
-		Window::RenderTexture(img, pos, nullptr, angle);
-		Window::RenderTexture(img, pos, nullptr, angle, 0, 0, SDL_FLIP_VERTICAL);
-
+		Window::RenderTexture(msg, msgBox);
+		Window::RenderTexture(ticks, ticksBox);
 		Window::Present();
 	}
 
-	SDL_DestroyTexture(img);
 	SDL_DestroyTexture(msg);
+	SDL_DestroyTexture(ticks);
 
 	Window::Quit();
 	return 0;
