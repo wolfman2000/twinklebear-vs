f180a882b3f8a813bea333239b2147cc22d9f7c4
Jason Felds <jafelds@gmail.com>
2014-02-14 22:33:08 -0500

    Allow [un]pausing the timer.

--- SDL_Tutorial/Timer.cpp
+++ SDL_Tutorial/Timer.cpp
@@ -14,3 +14,18 @@ void Timer::Stop() {
 	_isStarted = false;
 	_isPaused = false;
 }
+
+void Timer::Pause() {
+	if (_isStarted && !_isPaused) {
+		_isPaused = true;
+		_pausedTicks = SDL_GetTicks() - _startTicks;
+	}
+}
+
+void Timer::Unpause() {
+	if (_isPaused) {
+		_isPaused = false;
+		_startTicks = SDL_GetTicks() - _pausedTicks;
+		_pausedTicks = 0;
+	}
+}
